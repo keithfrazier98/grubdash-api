f6f1f278f16cc9fa697a60ee8f95bee79f35a560
Keith Frazier <keithers98@gmail.com>
2021-05-08 23:16:11 -0700

    added .all to routers

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -4,7 +4,7 @@ const controller = require("./dishes.controller");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
 
-router.route("/").get(controller.list).post(controller.create);
-router.route("/:dishId").get(controller.read).delete(methodNotAllowed).put(controller.update)
+router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed)
+router.route("/:dishId").get(controller.read).put(controller.update).all(methodNotAllowed)
 
 module.exports = router;

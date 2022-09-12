6bf9df6c82e8cbc08dbf32b4913ef3890a1dfe78
Keith Frazier <keithers98@gmail.com>
2022-09-12 14:12:10 -0700

    move express setup to index file

--- index.ts
+++ index.ts
@@ -1,6 +1,26 @@
 const { PORT = 5000 } = process.env;
 
-import app from './src/app'
+import express from "express";
+import cors from "cors";
+
+import errorHandler from "./src/errors/errorHandler";
+import notFound from "./src/errors/notFound";
+import ordersRouter from "./api/orders/router";
+import dishesRouter from "./api/dishes/router";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/dishes", dishesRouter);
+app.use("/orders", ordersRouter);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 app.listen(PORT, listener);
+
+export default app;
--- public/index.js
+++ public/index.js
@@ -4,7 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var _a = process.env.PORT, PORT = _a === void 0 ? 5000 : _a;
-var app_1 = __importDefault(require("./src/app"));
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var errorHandler_1 = __importDefault(require("./src/errors/errorHandler"));
+var notFound_1 = __importDefault(require("./src/errors/notFound"));
+var router_1 = __importDefault(require("./api/orders/router"));
+var router_2 = __importDefault(require("./api/dishes/router"));
+var app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/dishes", router_2.default);
+app.use("/orders", router_1.default);
+app.use(notFound_1.default);
+app.use(errorHandler_1.default);
 var listener = function () { return console.log("Listening on Port ".concat(PORT, "!")); };
-app_1.default.listen(PORT, listener);
+app.listen(PORT, listener);
+exports.default = app;
 //# sourceMappingURL=index.js.map
